b2b5204c31060d45bd4716d894e24972d062ec9a
emejota2 <emejota02@gmail.com>
2022-09-03 14:33:03 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UserProvider from './store/store';
+import UserProvider from './Store/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
