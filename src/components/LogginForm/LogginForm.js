50fb0af824a773a69609be86746e45219b29afb3
emejota2 <emejota02@gmail.com>
2022-09-03 14:29:06 -0300

    fixes

--- src/components/LogginForm/LogginForm.js
+++ src/components/LogginForm/LogginForm.js
@@ -10,12 +10,12 @@ const LogginForm = () => {
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5 ">
         <div class="bg-dark text-white bg-opacity-50" >
-          <div class="card-body p-5 text-center">
+          <div class="card-body p-5 ">
 
             <div class="mb-md-5 mt-md-4 pb-5">
 
-              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-              <p class="text-white-50 mb-5">Please enter your user and password!</p>
+              <h2 class="fw-bold mb-2 text-uppercase text-center">Login</h2>
+              <p class="text-white-50 mb-5 text-center">Please enter your user and password!</p>
 
               <div class="form-outline form-white mb-4">
                 <label class="form-label" for="typeEmailX">User</label>
@@ -29,7 +29,7 @@ const LogginForm = () => {
 
               <p class="small mb-5 pb-lg-2"><Link class="text-white-50" to="#!">Forgot password?</Link></p>
 
-              <button class="btn btn-outline-light btn-lg px-5 logginBoton" type="submit">Login</button>
+              <button class="btn btn-outline-light btn-lg logginBoton" type="submit">Login</button>
 
               <div class="d-flex justify-content-center text-center mt-4 pt-1">
                 <Link to="#!" class="text-white me-4"><i class="bi bi-facebook"></i></Link>
@@ -40,7 +40,7 @@ const LogginForm = () => {
             </div>
 
             <div>
-              <p class="mb-0">Don't have an account? <Link to="/signup" class="text-white-50 fw-bold">Sign Up</Link>
+              <p class="mb-0 text-center">Don't have an account? <Link to="/signup" class="text-white-50 fw-bold">Sign Up</Link>
               </p>
             </div>
 
