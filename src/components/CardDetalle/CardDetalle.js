a1ea2b699ad732c89809885178404c8c98bfcc51
emejota2 <emejota02@gmail.com>
2022-09-06 18:25:21 -0300

    creando la card para la pagina de detalle

--- src/components/CardDetalle/CardDetalle.js
+++ src/components/CardDetalle/CardDetalle.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react'
-import { Context } from "../../Store/Store";
 import { Link } from 'react-router-dom';
+import { Context } from "../../Store/Store";
 import './CardDetalle.css'
 
 const CardDetalle = (props) => {
     const {favAdd, favoritos} = useContext(Context)
-    const {description, image, information, stock, price, model, id, vape} = props
+    const {description, image, information, stock, price, model, id, producto} = props
     const isfav = favoritos.some(fav=> fav.id===id)
     const [label, emoji] = isfav?['remove', '❤️']:['add', '🤍']
 
     return (
 
-    <div className="card" style={{width: "60rem"}}>
+    <div className="card-detalle" style={{width: "60rem"}}>
         <img className="card-img-top" src={image} alt="Card image"/>
             <div className="card-body">
                     <h4 className="card-title">{model}</h4>
@@ -21,8 +21,8 @@ const CardDetalle = (props) => {
                     <p className=''>Unidades disponibles: {stock}</p>
                     <div className='d-grid gap-2 d-md-flex col-6'>
                     <button type="button" className="btn btn-primary btn-md">Agregar al carrito</button>
-                    <button className='' onClick={() => favAdd(vape) }> <span aria-label={label} role='img'>{emoji}</span></button>
-                    <button className='btn btn-primary' to='/'>Volver</button>
+                    <button className='' onClick={() => favAdd(producto) }> <span aria-label={label} role='img'>{emoji}</span></button>
+                    <Link to='/'><button className='btn btn-primary'  >Volver</button></Link>
                     </div>
             </div>
     </div>
