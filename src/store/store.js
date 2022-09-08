9d53d7cca6591cfa7e1cc8d9b5d935a2d23cf171
marcusty15 <marco.troconis15@gmail.com>
2022-09-08 19:06:30 -0400

    se agrego funcionalidad a carrito de compras

--- src/store/store.js
+++ src/store/store.js
@@ -1,113 +1,150 @@
-import axios from 'axios';
-import { createContext, useEffect, useState } from 'react';
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const Context = createContext(null);
-const UserProvider = ({children}) =>{
-    const [userinfo, setUserinfo] = useState({
-      first_name:'',
-      last_name:'',
-      username:'',
-      password:'',
-      gender:'',
-      birthdate:'',
-      address:'',
-      country:'',
-      province:'',
-      city:'',
-      zip:'',
-      email:'',
-      phone_code:'',
-      telephone:'',
-      tc:''
-    })
+const UserProvider = ({ children }) => {
+  const [userinfo, setUserinfo] = useState({
+    first_name: "",
+    last_name: "",
+    username: "",
+    password: "",
+    gender: "",
+    birthdate: "",
+    address: "",
+    country: "",
+    province: "",
+    city: "",
+    zip: "",
+    email: "",
+    phone_code: "",
+    telephone: "",
+    tc: "",
+  });
 
-    const data = userinfo
-    const [price, setPrice] = useState([])
-    const [producto, setProducto] = useState([]);
-    const [favoritos, setFavoritos] = useState([])
-    const [cart, setCart] = useState([])
-    const [fav, setFav] = useState(0)
-    const [item, setItem] = useState(1)
-// Funcion para obtener los productos guardados en la BD
-    const obtenerdatos = async () => {
-      const response = await axios.get(
-        `https://backendvape.herokuapp.com/products`
-      );
-      setProducto(response.data.data);
-      console.log(response.data.data);
-    };
+  const data = userinfo;
+  const [price, setPrice] = useState([]);
+  const [producto, setProducto] = useState([]);
+  const [favoritos, setFavoritos] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [fav, setFav] = useState(0);
+  const [item, setItem] = useState(1);
+  // Funcion para obtener los productos guardados en la BD
+  const obtenerdatos = async () => {
+    const response = await axios.get(
+      `https://backendvape.herokuapp.com/products`
+    );
+    setProducto(response.data.data);
+    console.log(response.data.data);
+  };
 
-//Funcion para poder enviar el formulario de registro a la BD para la creacion de usuarios
-    const crearDatos = async ()=>{
-      const response = await axios.post(`https://backendvape.herokuapp.com/users/signup`, data)
-      console.log(response)
-      
+  //Funcion para poder enviar el formulario de registro a la BD para la creacion de usuarios
+  const crearDatos = async () => {
+    const response = await axios.post(
+      `https://backendvape.herokuapp.com/users/signup`,
+      data
+    );
+    console.log(response);
+  };
+  //Funcion para agregar favoritos
+  const favAdd = (vape) => {
+    let addvape = favoritos.find((m) => m.id === vape.id);
+
+    if (addvape) {
+      alert("El vape ya esta agregada");
+      setFavoritos([...favoritos]);
+    } else {
+      setFav(fav + 1);
+      setFavoritos([...favoritos, { ...vape }]);
     }
-//Funcion para agregar favoritos
-    const favAdd= (vape) => {
-      let addvape = favoritos.find(m => m.id === vape.id);
-      
-      if(addvape){
-        alert("El vape ya esta agregada")
-        setFavoritos([...favoritos])
-  
-      } else{
-        setFav(fav+1)
-        setFavoritos([...favoritos, {...vape}])
-      }
-    };
-//Funcion para borrar favoritos
-    const borrarfav = (id) => {
-      setFav(fav-1)
-      return setFavoritos(favoritos.filter((m) => m.id !== id))
-  }
+  };
+  //Funcion para borrar favoritos
+  const borrarfav = (id) => {
+    setFav(fav - 1);
+    return setFavoritos(favoritos.filter((m) => m.id !== id));
+  };
 
-// Funcion para captar los valores en entrada en el formulario de registro
-  const handleChange = async (e)=>{
+  // Funcion para captar los valores en entrada en el formulario de registro
+  const handleChange = async (e) => {
     await setUserinfo({
-      
-        ...userinfo,
-        [e.target.name]: e.target.value
-      
-    })
-  }
+      ...userinfo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   //Funcion para agregar al carrito
-  const cartAdd= (vape) => {
-    let addvape = cart.find(m => m.id === vape.id);
-    
-    if(addvape){
-      alert("El vape ya esta agregado al carrito")
-      setCart([...cart])
+  const cartAdd = (vape) => {
+    let addvape = cart.find((m) => m.id === vape.id);
 
-    } else{
-      
-      setCart([...cart, {...vape}])
+    if (addvape) {
+      alert("El vape ya esta agregado al carrito");
+      setCart([...cart]);
+    } else {
+      setCart([...cart, { ...vape, quantity: 1 }]);
     }
+    console.log(cart);
   };
   //funcion para borrar articulos en el carrito
   const deleteCart = (id) => {
-    
-    return setCart(cart.filter((m) => m.id !== id))
-}
+    return setCart(cart.filter((m) => m.id !== id));
+  };
 
-const plusitem = () =>{
-   setItem(item + 1)
-}
-const minusItem = ()=>{
-  setItem(item - 1)
-}
-    useEffect(() => {
-      obtenerdatos();
-    }, []);
+  // funcion para agregar productos dentro del carrito
+  const increaseQuantityById = (vape) => {
+    const buscar = cart.find((product) => product.id === vape.id);
+    console.log(buscar);
+    if (buscar) {
+      setCart(
+        cart.map((product) => {
+          if (product.id === vape.id) {
+            return { ...buscar, quantity: buscar.quantity + 1 };
+          } else return product
+        })
+      );
+    } else {
+      setCart([...cart, { ...vape, quantity: 1 }]);
+    }
+  };
+  const deleteItemFromCart = (vape) => {
+    const buscar = cart.find((product) => product.id === vape.id);
+    console.log(buscar);
+    if (buscar) {
+      setCart(
+        cart.map((product) => {
+          if (product.id === vape.id) {
+            return { ...buscar, quantity: buscar.quantity - 1 };
+          } else return product
+        })
+      );
+    } else {
+      setCart([...cart, { ...vape.id, quantity: 1 }]);
+    }
+  };
 
+  useEffect(() => {
+    obtenerdatos();
+  }, []);
 
-    return(
-        <Context.Provider value={{ crearDatos ,handleChange ,producto, favAdd, borrarfav, setFavoritos, favoritos, cartAdd, deleteCart, cart, plusitem, minusItem, item}}>
-            {children}
-        </Context.Provider>
-    )
-}
+  return (
+    <Context.Provider
+      value={{
+        increaseQuantityById,
+        deleteItemFromCart,
+        crearDatos,
+        handleChange,
+        producto,
+        favAdd,
+        borrarfav,
+        setFavoritos,
+        favoritos,
+        cartAdd,
+        deleteCart,
+        cart,
+        item,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
 
 export default UserProvider;
-
