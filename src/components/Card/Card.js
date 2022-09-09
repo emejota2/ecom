6a3cfdae2febf4b8b7a0fe6ba8936e5ff5891779
emejota2 <emejota02@gmail.com>
2022-09-09 01:00:56 -0300

    trabajo en progreso con los botones de las cards normales, lo deje asi porque tengo suenio

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,39 +4,40 @@ import { Link } from "react-router-dom";
 import "./Card.css";
 import { Vapes } from "../../pages/Vapes/Vapes";
 
-
 const Card = (props) => {
-  const {favAdd, favoritos, cartAdd, borrarfav} = useContext(Context)
-  const {brand, model, price, image, information, id, vape}= props
-  const isfav = favoritos.some(fav=> fav.id===id)
-  const [label, emoji] = isfav?['remove', '❤️']:['add', '🤍']
+  const { favAdd, favoritos, cartAdd, borrarfav, deleteCart } = useContext(Context);
+  const { brand, model, price, image, information, id, vape } = props;
+  const isfav = favoritos.some((fav) => fav.id === id);
 
   return (
-    <div>
-      <div className="product-card">
-        <div className="badge">Hot</div>
-        <div className="product-tumb">
-          <Link to={`/detalle/${id}`}><img src={image} alt="" /></Link>
-        </div>
-        <div className="product-details">
-          <span className="product-catagory">{brand}</span>
-          <h4>
-            <Link to="/vape">{model}</Link>
-          </h4>
-          <h4 className="info">
-            {information}
-          </h4>
-          <div className="product-bottom-details">
-            <div className="product-price">
-            Precio: ${price}
-            </div>
-            <div className="product-links">
-              {favoritos.some(fav => fav.id===id) ? <i className="bi bi-heart-fill text-danger fs-4 ms-2" onClick={() => borrarfav(id)} ></i> : <i className="  bi bi-heart-fill text-white fs-4 ms-2" onClick={() => favAdd(vape)} ></i>}
-            <button className='fav' onClick={() => favAdd(vape) }> <span aria-label={label} role='img'>{emoji}</span></button>
-            <button className='fav' onClick={() => cartAdd(vape) }> <i className="bi bi-cart"></i></button>
-              
-            </div>
+    <div id="container">
+      <div class="cardGen">
+        <Link to={`/detalle/${id}`}><img src={image} alt="Lago di Braies" className="imgCard" /></Link>
+        <div class="card__details">
+          <span class="tag">{brand}</span>
+
+          <div class="name">{model}</div>
+
+          <p className="infoTag mt-3">{information}</p>
+          <p className="infoTag">Precio: ${price}</p>
+
+          <div>
+            <button className="bi bi-cart3" onClick={() => cartAdd(vape)}>
+              {" "}
+              <i className="bi bi-cart3"></i>
+            </button>
           </div>
+          {favoritos.some((fav) => fav.id === id) ? (
+            <i
+              className="bi bi-heart-fill text-danger"
+              onClick={() => borrarfav(id)}
+            ></i>
+          ) : (
+            <i
+              className="bi bi-heart text-dark"
+              onClick={() => favAdd(vape)}
+            ></i>
+          )}
         </div>
       </div>
     </div>
